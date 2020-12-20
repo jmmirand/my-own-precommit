861e279e156350ec1d643a0ca4968c51b375d46f
Jose Miguel <jmmirand@gmail.com>
2020-12-20 16:36:46 +0100

    fix

--- create-md-toc.sh
+++ create-md-toc.sh
@@ -5,10 +5,10 @@ for filepath in "$@"
 do
     echo "Username - $i: $filepath";
     i=$((i + 1));
-    FILE_LIST= "$FILE_LIST $filepath" 
+    FILE_LIST="$filepath" 
 done
 
-echo "$FILE_LIST"
+echo "LISTA : $FILE_LIST"
 
 gh-md-toc 
 
